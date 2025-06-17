d35d3802bea788e91de4fa91fa7c0ff7eb105924
Roopa9401 <roopashree9401@gmail.com>
2025-06-17 22:13:03 +0530

    adding files

---
+++ lwcComp4_Iteration.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class LwcComp4_Iteration extends LightningElement {
+
+    list=[
+        {id:1,Task:'Learn LWC',Status:'Completed'},
+        {id:2,Task:'Create a new LWC',Status:'In Progress'},
+        {id:3,Task:'Style the LWC',Status:'In Progress'}
+    ]
+
+    
+}
